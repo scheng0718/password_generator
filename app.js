48a526ebb9a632161528ef1c5dab1edb2eefcbd6
scheng0718 <scheng0718@gmail.com>
2023-05-24 13:02:37 -0700

    Set up route for POST method and import the body-parser package in express

--- app.js
+++ app.js
@@ -6,13 +6,24 @@ const app = express()
 const port = 3000
 // Load express-handlebars
 const exphdb = require('express-handlebars')
+// Load body parser package
+const bodyParser = require('body-parser')
 
 // Use express handlebars in express
 app.engine('handlebars', exphdb({ defaultLayout: 'main'}))
 app.set('view engine', 'handlebars') 
 
+// Use body parser
+app.use(express.urlencoded({extended: true}))
+
 // Setting up route and response body 
 app.get('/', (req, res) => {
+  res.render('index')
+})
+// Setting up POST route
+app.post('/', (req, res) => {
+  // console.log('This is a response from POST')
+  console.log('req.body', req.body)
   res.render('index')
 })
 
